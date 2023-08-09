2a4a9856844573c79db8e5561515a2cbe6317804
yanghuanlie <yanghuanlie@mail.shareintelli.com>
2023-08-09 18:11:22 +0800

    textView设置夜间颜色

--- lib/src/main/java/com/daimacool/autothemeview/view/ThemeTextView.kt
+++ lib/src/main/java/com/daimacool/autothemeview/view/ThemeTextView.kt
@@ -1,20 +1,73 @@
 package com.daimacool.autothemeview.view
 
 import android.content.Context
+import android.content.res.ColorStateList
 import android.util.AttributeSet
+import com.daimacool.autothemeview.R
 
 class ThemeTextView : androidx.appcompat.widget.AppCompatTextView {
 
-    constructor(context: Context) : super(context)
+    private var textNightColor: ColorStateList? = null
 
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    private var lightTextColor: ColorStateList? = null
+
+    private var initialized = false
+
+    constructor(context: Context) : this(context, null)
+
+    constructor(context: Context, attrs: AttributeSet?) : this(
+        context,
+        attrs,
+        android.R.attr.textViewStyle
+    )
 
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
         context,
         attrs,
         defStyleAttr
-    )
+    ) {
+        val typedArray =
+            context.theme.obtainStyledAttributes(attrs, R.styleable.ThemeTextView, defStyleAttr, 0)
+        for (i in 0 until typedArray.indexCount) {
+            val index = typedArray.getIndex(i)
+            if (index == R.styleable.ThemeTextView_theme_textDarkColor) {
+                textNightColor = typedArray.getColorStateList(index)
+            }
+        }
+        initialized = true
+        applyTextThemeColor()
+    }
 
+    override fun setTextAppearance(resId: Int) {
+        super.setTextAppearance(resId)
+        applyTextThemeColor()
+    }
 
+    override fun setTextColor(color: Int) {
+        if (initialized.not()) {
+            lightTextColor = ColorStateList.valueOf(color)
+            return
+        }
+        super.setTextColor(color)
+    }
 
+    override fun setTextColor(colors: ColorStateList?) {
+        if (initialized.not()) {
+            lightTextColor = colors
+            return
+        }
+        super.setTextColor(colors)
+    }
+
+    private fun applyTextThemeColor() {
+        // todo aa
+        val isDark = true
+        if (isDark && textNightColor != null) {
+            setTextColor(textNightColor)
+        } else {
+            lightTextColor?.let {
+                setTextColor(it)
+            }
+        }
+    }
 }
